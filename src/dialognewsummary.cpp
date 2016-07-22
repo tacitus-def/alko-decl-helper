bfd66a56053d64b16adaa65787c3f899cacfe587
Petr Sleptsov <spetr@bk.ru>
2016-07-22 11:04:05 +0900

    Добавлена информация про используемую лицензию GPL3 и автора вместе с его электронной почтой в файлы *.cpp и *.h

--- src/dialognewsummary.cpp
+++ src/dialognewsummary.cpp
@@ -1,3 +1,22 @@
+//
+//  Copyright (c) 2016 Petr Sleptsov <spetr@bk.ru>
+//
+//  This file is part of Alko-decl-helper.
+//
+//  Alko-decl-helper is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Alko-decl-helper is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Alko-decl-helper.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 #include "dialognewsummary.h"
 #include "ui_dialognewsummary.h"
 
